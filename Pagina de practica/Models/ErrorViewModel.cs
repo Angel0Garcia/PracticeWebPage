843704ce145a0c1c04c8fe6f16e840400bb9ce03
Angel0Garcia <gabrielg080nac@gmail.com>
2024-04-10 23:27:07 -0400

    commit dos

---
+++ Pagina de practica/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PracticeWebPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
